36e8818657e050b78dafaf74216afb215dd45c82
somrat sorkar <themefisher.dev@gmail.com>
2023-05-13 16:25:26 +0600

    added Tab Shortcode

--- src/layouts/shortcodes/Tabs.tsx
+++ src/layouts/shortcodes/Tabs.tsx
@@ -1,11 +1,10 @@
 import { marked } from "marked";
-import { number } from "prop-types";
-import React, {useState } from "react";
+import React, { useState } from "react";
 
 function Tabs({ children }: { children: any }) {
-const [active,setAvtive]=useState(0)
+  const [active, setAvtive] = useState(0);
 
-  const tabLinks = Array.from(
+  const tabItems = Array.from(
     children.props.value.matchAll(
       /<div\s+data-name="([^"]+)"[^>]*>(.*?)<\/div>/gs
     ),
@@ -15,23 +14,29 @@ const [active,setAvtive]=useState(0)
   return (
     <div className="tab">
       <ul className="tab-nav">
-        {tabLinks.map((item: any, index: number) => (
+        {tabItems.map((item: any, index: number) => (
           <li
             key={index}
             className={`tab-nav-item ${index === active && "active"}`}
-            onClick={() =>setAvtive(index) }
+            onClick={() => setAvtive(index)}
           >
             {item.name}
           </li>
         ))}
       </ul>
 
-
-      {
-        tabLinks.map((item,i)=><div className={active===i?"block tab-content px-5":"hidden"} dangerouslySetInnerHTML={{__html:marked.parseInline(item.children)}} />)
-      }
-
-
+      <div className="tab-content">
+        {tabItems.map((item, i) => (
+          <div
+            className={`tab-content-panel ${
+              active === i ? "active" : "hidden"
+            }`}
+            dangerouslySetInnerHTML={{
+              __html: marked.parse(item.children),
+            }}
+          />
+        ))}
+      </div>
     </div>
   );
 }
