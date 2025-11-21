f9533456ceb6d270abaf1c80f7e084b9d1001255
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-11-21 17:01:06 +0800

    更新 readingTime.ts

--- src/lib/utils/readingTime.ts
+++ src/lib/utils/readingTime.ts
@@ -28,12 +28,12 @@ const readingTime = (content: string): string => {
 
   if (minutes < 10) {
     if (minutes < 2) {
-      return "0" + minutes + ` Min read`;
+      return "0" + minutes + ` 分钟阅读`;
     } else {
-      return "0" + minutes + ` Mins read`;
+      return "0" + minutes + ` 分钟阅读`;
     }
   } else {
-    return minutes + ` Mins read`;
+    return minutes + ` 分钟阅读`;
   }
 };
 
